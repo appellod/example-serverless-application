85f71703bb72160349db0f91d8f4296cc5c8b047
Dan Appello <dappello@apto.com>
2018-02-09 16:19:28 -0700

    fix async describes

--- src/test/documentation/index.ts
+++ src/test/documentation/index.ts
@@ -13,7 +13,7 @@ const host = config.server.host;
 const port = config.server.port;
 
 describe("documentation/index.ts", function() {
-  describe("GET /apidoc/index.html", async function() {
+  describe("GET /apidoc/index.html", function() {
     it("does not return a response", async function() {
       const method = "get";
       const path = "/apidoc/index.html";
@@ -29,7 +29,7 @@ describe("documentation/index.ts", function() {
     });
   });
 
-  describe("GET /login.html", async function() {
+  describe("GET /login.html", function() {
     it("returns a 200 status code", async function() {
       const method = "get";
       const path = "/login.html";
