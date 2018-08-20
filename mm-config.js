c23cf815e9513395ea76836fc78475b3b8e5efac
Dan Appello <appellod@gmail.com>
2018-08-19 23:52:14 -0600

    remove token migration, update migrations directory

--- mm-config.js
+++ mm-config.js
@@ -1,7 +1,7 @@
 const conf = {
   collection: "schemaMigrations",
   db: process.env.MONGO_DATABASE,
-  directory: "mongodb-migrations",
+  directory: "./migrations/mongo",
   host: process.env.MONGO_HOST,
   port: process.env.MONGO_PORT
 };
